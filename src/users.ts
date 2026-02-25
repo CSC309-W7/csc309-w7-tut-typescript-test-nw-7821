1394d219ca57ea4200a8c4071a76ef8627bba1c2
nw-7821 <natweb187@gmail.com>
2026-02-25 18:50:25 -0500

    attempt #1 to solve

--- src/users.ts
+++ src/users.ts
@@ -7,7 +7,20 @@ export const apiResponse: unknown = [
 ];
 
 export function getUsersData(): User[] {
-  return apiResponse as User[]; // intentionally unsafe
+  // return apiResponse as User[]; // intentionally unsafe
+  for user in apiResponse {
+    if (typeof user.name !== string) {
+        user.name = String(user.name)
+    }
+
+    if (typeof user.age !== number) {
+        user.age = Number(user.age)
+    }
+
+  }
+
+  return apiResponse as User[];
+  
 }
 
 export function formatAges(users: User[]): string[] {
