bc111727c69175ead02b85850dffbf12809bc025
nw-7821 <natweb187@gmail.com>
2026-02-25 19:09:02 -0500

    Update users.ts to work

--- src/users.ts
+++ src/users.ts
@@ -1,19 +1,19 @@
 import type { User } from "./types";
 
-export const apiResponse: unknown = [
+export const apiResponse: User[] = [
   { name: "Tony", age: 23 },
-  { name: "Kevin", age: "24" }, // invalid
+  { name: "Kevin", age: 24 }, 
   { name: "Jim", age: 25 },
 ];
 
 export function getUsersData(): User[] {
   // return apiResponse as User[]; // intentionally unsafe
-  for user in apiResponse {
-    if (typeof user.name !== string) {
+  for (const user of apiResponse) {
+    if (typeof(user.name) != "string") {
         user.name = String(user.name)
     }
 
-    if (typeof user.age !== number) {
+    if (typeof user.age !== "number") {
         user.age = Number(user.age)
     }
 
